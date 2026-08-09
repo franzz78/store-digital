57ea4e902e13c4766fa22872e8a2d41faf6ebc68
Haruukii Dharmawijaya <rohjaifirdaus@gmail.com>
2026-08-10 00:37:41 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,9 +4,7 @@ const ASSETS_TO_CACHE = [
   './index.html',
   './style.css',
   './script.js',
-  './manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://cdn.jsdelivr.net/npm/sweetalert2@11'
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
@@ -17,24 +15,10 @@ self.addEventListener('install', event => {
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
-});
-
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      return cachedResponse || fetch(event.request).catch(() => {
-        if (event.request.mode === 'navigate') {
-          return caches.match('./index.html');
-        }
-      });
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
