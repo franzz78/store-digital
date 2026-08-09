e56cb5b86bcd08125962195f20d5da533bc3e4e5
Haruukii Dharmawijaya <rohjaifirdaus@gmail.com>
2026-08-10 00:45:54 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'store-logistik-v1';
-const ASSETS_TO_CACHE = [
+const ASSETS = [
   './',
   './index.html',
   './style.css',
@@ -7,18 +7,18 @@ const ASSETS_TO_CACHE = [
   './manifest.json'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS_TO_CACHE);
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
     })
   );
 });
